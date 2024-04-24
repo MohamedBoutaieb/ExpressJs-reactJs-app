5098104e3526dbe1b23fbd8c757e45ea91b3fc4a
mohamedBoutaieb <mohamed@letsfluence.com>
2024-04-24 18:10:50 +0200

    set index path to the dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,16 @@ import { Suspense } from "react";
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
-import { useAppDispatch } from "./config/store";
 import { ToastContainer } from "react-toastify";
 
 function App() {
-  const dispatch = useAppDispatch();
   return (
     <>
       <ToastContainer />
       <BrowserRouter>
-        {/* {users.length > 0 ? users[0].id : null} */}
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route path="/users" element={<Home />} />
+            <Route path="" element={<Home />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
